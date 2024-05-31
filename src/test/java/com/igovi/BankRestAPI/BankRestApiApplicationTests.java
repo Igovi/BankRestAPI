3f8a4a5f3247875dc9f550dd32c784e9afa353d9
Igovi <igovi9910@gmail.com>
2024-05-31 14:58:30 -0300

    Fist Commit

---
+++ src/test/java/com/igovi/BankRestAPI/BankRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igovi.BankRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
